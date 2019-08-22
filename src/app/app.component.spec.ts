06407d1001606d2d43bbd8df4b397f34ec413c08
Chad Hedgcock <chedgcock@i-parcel.com>
2019-08-22 17:52:46 -0400

    passing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Club, Meeting } from './club';
+
+import ClubJson from '../assets/club.json';
 
 describe('AppComponent', () => {
+
+  const club = ClubJson as Club;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -20,16 +26,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cheers'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cheers');
-  });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cheers!');
+    expect(compiled.querySelector('h3.display-5').textContent).toContain(club.name);
   });
 });
